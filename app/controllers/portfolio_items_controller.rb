7303428e1d15dd9c2bf5c4ca9170c04d8e746b99
Elizabeth <esimonian16@gmail.com>
2017-05-17 14:30:11 -0700

    add edit/new to portfolio items

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -1,11 +1,54 @@
 class PortfolioItemsController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = PortfolioItem.all
   end
 
-  def show
+  def new
+    @portfolio_item = PortfolioItem.new
+  end
+
+  def create
+    @portfolio_item = PortfolioItem.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_items_path, notice: 'Item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
     
   end
 
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolio_items_path, notice: 'portfolio_item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio_item
+      @portfolio_item = PortfolioItem.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_item_params
+      params.require(:portfolio_item).permit(:title, :subtitle, :body)
+    end
 
 end
