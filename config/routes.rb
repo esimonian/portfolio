fc85f689c28f3c5ae3a82fba071ebff6f6d0a0d0
Elizabeth <esimonian16@gmail.com>
2017-05-18 12:02:03 -0700

    added new params for user registration/edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   resources :portfolio_items, except: [:show]
   get 'portfolio/:id', to: 'portfolio_items#show', as: 'portfolio_item_show'
 
